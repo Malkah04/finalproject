a72b4930d274a1a7681a0454a0a120b243db0e6e
Malkah04 <malaaaaak52@gmail.com>
2024-12-12 16:01:21 +0200

    make the monster

--- src/Man/AnimListener.java
+++ src/Man/AnimListener.java
@@ -3,13 +3,10 @@ package Man;
 import java.awt.event.KeyListener;
 import javax.media.opengl.GLEventListener;
 
-/**
- *
- * @author Tyba
- */
+
 
 public abstract class AnimListener implements GLEventListener, KeyListener {
 
-    protected String assetsFolderName = "C:\\Users\\Esraa\\Downloads\\finalproject-master\\finalproject-master\\man\\player";
-
+//    protected String assetsFolderName = "C:\\Users\\Esraa\\Downloads\\finalproject-master\\finalproject-master\\man\\player";
+    protected String assetsFolderName = "C:\\Users\\malaa\\Downloads\\finalproject-master\\finalproject-master\\man\\player";
 }
