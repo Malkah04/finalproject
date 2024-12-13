0fdf71862e2b1767678e55a2866c56ae0c0ce5f3
Roaa Ahmed <roaahamza01@gmail.com>
2024-12-13 20:37:22 +0200

    time

--- src/Man/AnimListener.java
+++ src/Man/AnimListener.java
@@ -8,5 +8,5 @@ import javax.media.opengl.GLEventListener;
 
 public abstract class AnimListener implements GLEventListener, KeyListener, MouseListener {
 
-    protected  String assetsFolderName="C:\\Users\\RTX\\IdeaProjects\\finalproject\\src\\sounds";
+    protected  String assetsFolderName="C:\\graphicFinal\\src\\sounds";
 }
