f912b78ae0ebee41cfae69f5d18ad39b4ed42c40
julia <202227099@std.sci.cu.edu.eg>
2024-12-16 22:56:31 +0200

    multiplayer

--- src/Man/AnimListener.java
+++ src/Man/AnimListener.java
@@ -12,7 +12,7 @@ public abstract class AnimListener implements GLEventListener, KeyListener, Mous
 //    protected  String assetsFolderName="C:\\Users\\DELL\\IdeaProjects\\Final\\game\\src\\sounds";
 
 //     protected  String assetsFolderName="C:\\finalGraphic\\src\\sounds";
-     protected  String assetsFolderName="C:\\Users\\DELL\\IdeaProjects\\player\\finalproject\\src\\sounds";
+     protected  String assetsFolderName="C:\\Users\\c2d\\IdeaProjects\\finalproject\\src\\sounds";
 
 //    protected  String assetsFolderName="C:\\graphicFinal\\src\\sounds";
 }
