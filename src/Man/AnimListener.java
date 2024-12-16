b863413d2b912c8b3b59f0408ba7d9f5f73faa51
walaa <walaatarek59@gmail.com>
2024-12-16 20:59:24 +0200

    update game

--- src/Man/AnimListener.java
+++ src/Man/AnimListener.java
@@ -12,7 +12,7 @@ public abstract class AnimListener implements GLEventListener, KeyListener, Mous
 //    protected  String assetsFolderName="C:\\Users\\DELL\\IdeaProjects\\Final\\game\\src\\sounds";
 
 //     protected  String assetsFolderName="C:\\finalGraphic\\src\\sounds";
-     protected  String assetsFolderName="C:\\Users\\malaa\\IdeaProjects\\finalG\\src\\sounds";
+     protected  String assetsFolderName="C:\\Users\\DELL\\IdeaProjects\\logic\\finalproject\\src\\sounds";
 
 //    protected  String assetsFolderName="C:\\graphicFinal\\src\\sounds";
 }
